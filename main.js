99a4e99d230cfeeb8b82b09ee435dbc65ee3179c
Miguel <miguelbartz.profissional@gmail.com>
2024-03-26 20:19:41 -0300

    add function(addLinha)

---
+++ main.js
@@ -0,0 +1,36 @@
+const form = document.getElementById('form-contato');
+const contatos = [];
+const numeros = [];
+const imgContato = '<img src="./imgs/profile-user_64572.png" alt="foto do contato" />'
+
+let linhas = '';
+
+form.addEventListener('submit', function(e){ 
+    e.preventDefault();
+
+    addLinha();
+    refreshTable();
+    totalContatos();
+})
+
+function addLinha(){
+    const inputContato = document.getElementById('nome-contato');
+    const inputNumero = document.getElementById('numero-contato');
+
+    if (contatos.includes(inputContato.value)) {
+        alert(`Esse contato já existe. Verifique e tente novamente!`)
+    } else if (numeros.includes(inputNumero.value)) {
+        alert(`Já existe um contato com esse número. Verifique e tente novamente`)
+    } else {
+        contatos.push(inputContato.value);
+        numeros.push(inputNumero.value);
+
+        let linha = '<tr>';
+        linha += `<td>${imgContato}</td>`;
+        linha += `<td>${inputContato.value}</td>`;
+        linha += `<td>${inputNumero.value}</td>`;
+        linha += '</tr>';
+
+        linhas += linha;
+    }
+}
